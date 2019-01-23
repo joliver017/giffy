02e4d6249642d2e822de461a8823bc005f4a45a1
Oliver 2wist <joliver017@gmail.com>
2019-01-22 23:08:58 -0600

    added giphy API call

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,6 +15,21 @@ $(document).ready(function () {
 
     };
 
+    function displayGif() {
+        var searchTerm = $(this).attr("data-name")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RU65DZw7qnvqYSinQa9GLirT5CUivh5G&q=" + searchTerm + "&limit=25&offset=0&lang=en";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response){
+          console.log(response)
+         // $("#container").text(JSON.stringify(response))
+          $("#container").append("<iframe src=" + response.data[0].embed_url + "width='480' height='252' frameBorder='0' class='giphy-embed' allowFullscreen>" + "</iframe>")
+        })
+      };
+// <iframe src="https://giphy.com/embed/lXiRKBj0SAA0EWvbG" width="480" height="252" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    $(document).on("click", ".movie", displayGif);
     createButtons();
 
 });
