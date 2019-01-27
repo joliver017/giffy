56f4f859997e9d3a96a94a8a923da6fd9bbce32e
Oliver 2wist <joliver017@gmail.com>
2019-01-26 18:14:45 -0600

    fixed inserting the gif URL from Giphy API onto the HTML doc

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,15 +17,17 @@ $(document).ready(function () {
 
     function displayGif() {
         var searchTerm = $(this).attr("data-name")
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RU65DZw7qnvqYSinQa9GLirT5CUivh5G&q=" + searchTerm + "&limit=25&offset=0&lang=en";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=P4ydkWOBrq7IjD4grkWGBWusd2oTIfBI&limit=10";
 
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response){
           console.log(response)
+          console.log(searchTerm)
+          console.log(response.data[0].images.fixed_height.url)
          // $("#container").text(JSON.stringify(response))
-          $("#container").append("<img src=" + response.data[0].embed_url + "/>")
+          $("#container").prepend("<img src= '" + response.data[0].images.fixed_height.url + "' />")
         })
       };
 // <iframe src="https://giphy.com/embed/lXiRKBj0SAA0EWvbG" width="480" height="252" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
